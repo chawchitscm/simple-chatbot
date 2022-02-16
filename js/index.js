9387c7c20d351927ebed8ea93aed07298b62875d
Chaw Chit Su Thwe <scm.chawchitsuthwe@gmail.com>
2022-02-16 21:49:46 +0630

    refactored codes

--- js/index.js
+++ js/index.js
@@ -1,54 +1,79 @@
-$.getJSON("data/simple-data.json", function (jsonData) {
-  let message = document.createElement("li");
-  message.innerText = jsonData[0].text;
-  message.classList.add("bot");
-  document.getElementById("chatbox").appendChild(message);
+const startId = 1;
 
-  if (jsonData[0].messageType == "Question") {
-    if (jsonData[0].options.length > 0) {
-      let reply = document.createElement("li");
-      let optionsEle = document.createElement("ul");
-      for (let option = 0; option < jsonData[0].options.length; option++) {
-        let optionEle = document.createElement("li");
-        let link = document.createElement("a");
-        link.innerText = jsonData[0].options[option].option;
-        link.classList.add("option");
-        link.setAttribute("data-nextmessageid", jsonData[0].options[option].option_nextMessageId);
-        link.addEventListener("click", goToNextMessage);
-        optionEle.appendChild(link);
-        optionsEle.appendChild(optionEle);
-      }
-      reply.appendChild(optionsEle);
-      reply.classList.add("reply");
-      document.getElementById("chatbox").appendChild(reply);
+let container = document.createElement("div");
+container.classList.add("container");
+let chatbox = document.createElement("ul");
+chatbox.setAttribute("id", "chatbox");
+container.appendChild(chatbox);
+document.body.appendChild(container);
+
+document.getElementById("start").addEventListener("click", function() {
+  document.getElementById("start").remove();
+  $.getJSON("data/simple-data.json", function (jsonData) {
+    startChat(jsonData);
+  });
+});
+
+function startChat(data) {
+  var message = findMessageInJsonById(data, startId);
+  generateMessage(data, message)
+}
+
+function findMessageInJsonById(data, id) {
+  for (let index = 0; index < data.length; index++) {
+    if (data[index].id == id) {
+      return data[index];
     }
   }
-  
-  function goToNextMessage(event) {
-    let messageId = event.target.getAttribute("data-nextmessageid");
-    let message = document.createElement("li");
-    message.innerText = jsonData[messageId-1].text;
-    message.classList.add("bot");
-    document.getElementById("chatbox").appendChild(message);
+}
 
-    if (jsonData[messageId-1].messageType == "Question") {
-      if (jsonData[messageId-1].options.length > 0) {
-        let reply = document.createElement("li");
+function generateMessage(data, message) {
+  let audioCls = null;
+  if (message.audioPath) {
+    audioCls = new Audio(message.audioPath);
+  }
+  if (audioCls) {
+    audioCls.play();
+  }
+  let messageEle = document.createElement("li");
+  messageEle.innerText = message.text;
+  messageEle.classList.add("bot");
+  chatbox.appendChild(messageEle);
+
+  if (message.messageType == "Question") {
+    if (message.options) {
+      if (message.options.length > 0) {
+        let replyEle = document.createElement("li");
         let optionsEle = document.createElement("ul");
-        for (let option = 0; option < jsonData[messageId-1].options.length; option++) {
+        for (let option = 0; option < message.options.length; option++) {
           let optionEle = document.createElement("li");
           let link = document.createElement("a");
-          link.innerText = jsonData[messageId-1].options[option].option;
+          link.innerText = message.options[option].option;
           link.classList.add("option");
-          link.setAttribute("data-nextmessageid", jsonData[messageId-1].options[option].option_nextMessageId);
-          link.addEventListener("click", goToNextMessage);
+          link.setAttribute("data-nextmessageid", message.options[option].option_nextMessageId);
+          link.addEventListener("click", function(event) {
+            let nextMessageId = event.target.getAttribute("data-nextmessageid");
+            message = findMessageInJsonById(data, nextMessageId);
+            setTimeout(function() {
+              generateMessage(data, message);
+            }, 1500);
+          });
           optionEle.appendChild(link);
           optionsEle.appendChild(optionEle);
         }
-        reply.appendChild(optionsEle);
-        reply.classList.add("reply");
-        document.getElementById("chatbox").appendChild(reply);
+        setTimeout(function() {
+          replyEle.appendChild(optionsEle);
+          replyEle.classList.add("reply");
+          chatbox.appendChild(replyEle);
+        }, 1500);
       }
     }
   }
-});
+
+  setTimeout(function() {
+    if (message.nextMessageId != "") {
+      var nextMessage = findMessageInJsonById(data, message.nextMessageId);
+      generateMessage(data, nextMessage);
+    }
+  }, 1500);
+}
